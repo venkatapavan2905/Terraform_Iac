a35d7e3644fdc4336c450770cf3ee040c6870b24
Venkata Pavan <venkatapavan2905@gmail.com>
2024-02-18 14:38:26 +0530

    Made final changes

--- AWS_Iac/main.tf
+++ AWS_Iac/main.tf
@@ -69,6 +69,7 @@ resource "aws_launch_template" "ec2_template" {
 #Load balancer
 resource "aws_lb" "LoadBalancer_ALB" {
     load_balancer_type = "application"
+    internal = false
     subnets = [aws_subnet.pub_sub1.id]
     security_groups = [aws_security_group.Loadbalancer_SG.id]
 }
@@ -91,7 +92,7 @@ resource "aws_lb_target_group" "Target_Instances" {
 }
 
 #Load balancer listener rule
-resource "aws_lb_listener_rule" "asg" {
+/*resource "aws_lb_listener_rule" "asg" {
   listener_arn = aws_lb_listener.http.arn
   priority     = 100
   condition {
@@ -103,31 +104,44 @@ resource "aws_lb_listener_rule" "asg" {
    type            ="forward"
    target_group_arn=aws_lb_target_group.Target_Instances.arn
   }
-}
+}*/
 
-#Load balancer listener
+#Load balancer listener for http
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.LoadBalancer_ALB.arn
   port              = 80
   protocol          = "HTTP"
   default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
+    type = "forward"
+    target_group_arn = aws_lb_target_group.Target_Instances.arn
   }
 }
 
+#Load balancer listener for https
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.LoadBalancer_ALB.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = "arn:aws:acm:ap-south-1:113066798821:certificate/038ac9bb-1aa4-4f5f-9862-97653d478f00"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.Target_Instances.arn
+  }
+}
+
+
 #Auto scaling group
-resource "aws_autoscaling_group" "demo" {
+resource "aws_autoscaling_group" "Project-ASG" {
+  name = "Project-ASG"
   launch_template {
     id = aws_launch_template.ec2_template.id
   }
   vpc_zone_identifier = [aws_subnet.pvt_sub1.id, aws_subnet.pvt_sub2.id, aws_subnet.pvt_sub3.id]
   target_group_arns = [aws_lb_target_group.Target_Instances.arn]
-  #health_check_type = "ELB"
+  health_check_type = "ELB"
+  desired_capacity = 1
   min_size = 1
   max_size = 2
   tag {
@@ -137,9 +151,16 @@ resource "aws_autoscaling_group" "demo" {
   }
 }
 
-#route53 zone creation
+/*
+# Attaching Target groups to ALB
+resource "aws_autoscaling_attachment" "example_asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.Project-ASG.name
+  target_group_arns  = aws_lb_target_group.Target_Instances.arn
+}
+
+/*#route53 zone creation
 resource "aws_route53_zone" "dns_zone" {
-  name = "yvpinfo.xyz"
+  name = "terraiacproj.yvpinfo.xyz"
 }
 
 #route53 record creation
@@ -150,7 +171,7 @@ resource "aws_route53_record" "ns_record" {
   ttl     = 300
   records = [aws_lb.LoadBalancer_ALB.dns_name]
   
-}
+}*/
 
 output "load-balancer-dns-name" {
   value = aws_lb.LoadBalancer_ALB.dns_name
